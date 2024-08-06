765a23f3865ead79facf1dee83b59ee7de853707
bhuvan-perakam <bhuvan.perakam@trextms.com>
2024-08-06 08:03:11 +0100

    Fixed references for transfers component

--- Web/src/app/demo/components/transfers/transfers.module.ts
+++ Web/src/app/demo/components/transfers/transfers.module.ts
@@ -1,13 +1,14 @@
-import { CUSTOM_ELEMENTS_SCHEMA,NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { PanelModule } from 'primeng/panel';
+import { TabViewModule  } from 'primeng/tabview';
+import { DropdownModule } from "primeng/dropdown";
+import { InputTextModule } from "primeng/inputtext";
+import { CalendarModule } from "primeng/calendar";
+
 import { TransfersComponent } from './transfers.component';
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
-import { ButtonsModule } from '@progress/kendo-angular-buttons';
-import { InputsModule } from '@progress/kendo-angular-inputs';
-import { TabStripModule } from '@progress/kendo-angular-layout';
-import { GridModule } from '@progress/kendo-angular-grid';
 import { TransfersRoutingModule } from './transfers-routing.module'; 
 
 @NgModule({
@@ -17,15 +18,14 @@ import { TransfersRoutingModule } from './transfers-routing.module';
   imports: [
     CommonModule,
     FormsModule,
-    DropDownsModule,
-    DateInputsModule,
-    ButtonsModule,
-    InputsModule,
-    TabStripModule,
-    GridModule,
-    TransfersRoutingModule 
+    TransfersRoutingModule ,
+
+    PanelModule,
+    TabViewModule,
+    DropdownModule,
+    InputTextModule,
+    CalendarModule
   ],
-  exports: [TransfersComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  exports: [TransfersComponent]
 })
 export class TransfersModule { }
