bcc4e32ad384fdce8cf9076a5c5e62108fa1a226
bhuvan-perakam <bhuvan.perakam@trextms.com>
2025-03-14 15:36:57 +0000

    Removed duplicate menu items

--- Web/src/app/layout/app.menu.component.ts
+++ Web/src/app/layout/app.menu.component.ts
@@ -100,88 +100,6 @@ export class AppMenuComponent implements OnInit {
                 ],
             },
             {
-                label: 'Treasury Deals',
-                items: [
-                    {
-                        label: 'Investment Portfolio', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label: 'Deposits', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label: 'Funds', icon: 'pi pi-fw pi-bookmark'
-                    },
-                ],
-            },
-            {
-                label: 'Asset & Liability Management',
-                items: [
-                    {
-                        label: 'Assets', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    
-                ],
-            },
-            {
-                label: 'Forecasting',
-                items: [
-                    {
-                        label: 'Cash Forecast', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    
-                ],
-            },
-            {
-                label: 'Invoice Management',
-                items: [
-                    {
-                        label: 'Invoices', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    
-                ],
-            },
-            {
-                label: 'Treasury Deals',
-                items: [
-                    {
-                        label: 'Investment Portfolio', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label: 'Deposits', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label: 'Funds', icon: 'pi pi-fw pi-bookmark'
-                    },
-                ],
-            },
-            {
-                label: 'Asset & Liability Management',
-                items: [
-                    {
-                        label: 'Assets', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    
-                ],
-            },
-            {
-                label: 'Forecasting',
-                items: [
-                    {
-                        label: 'Cash Forecast', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    
-                ],
-            },
-            {
-                label: 'Invoice Management',
-                items: [
-                    {
-                        label: 'Invoices', icon: 'pi pi-fw pi-bookmark'
-                    },
-                    
-                ],
-            },
-            {
                 label: 'Administration',
                 items: [
                     {
@@ -189,9 +107,6 @@ export class AppMenuComponent implements OnInit {
                     },
                     {
                         label: 'Role Management', icon: 'pi pi-fw pi-bookmark', routerLink: ['/settings/rolemanagement']
-                    },
-                    {
-                        label: 'Role Management', icon: 'pi pi-fw pi-bookmark'
                     },
                     {
                         label: 'Permissions', icon: 'pi pi-fw pi-bookmark', routerLink: ['/settings/permissions']
