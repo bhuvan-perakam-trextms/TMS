f5761c8c51b5cda622abb1d02eaf4767c9414421
bhuvan-perakam-trextms <bhuvan.perakam@trextms.com>
2025-03-16 17:58:38 +0000

    Reports - Baseline Implementation (#26)
    * Removed duplicate menu items
    
    * Implemented Reports - Iteration - 1

---
+++ Web/src/app/demo/components/reporting/reports-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReportsComponent } from './reports/reports.component';
+import { ReportbuilderComponent } from './reportbuilder/reportbuilder.component';
+
+@NgModule({
+    imports: [RouterModule.forChild([
+        { path: '', component: ReportsComponent },
+        { path: 'reportbuilder', component: ReportbuilderComponent }
+    ])],
+    exports: [RouterModule]
+})
+export class ReportsRoutingModule { }
